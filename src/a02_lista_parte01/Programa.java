8a5591b8ef467026f032db989170fdf3f71c30c5
Marco Melo <josemarcomelonascimento@gmail.com>
2024-07-15 16:10:54 -0300

    Commit inicial

---
+++ src/a02_lista_parte01/Programa.java
@@ -0,0 +1,12 @@
+package a02_lista_parte01;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Aluno aluno01 = new Aluno("Marco01", "marco01@email.com");
+		System.out.println(aluno01);
+		Aluno aluno02 = new Aluno("Marco02", "marco02@email.com");
+		System.out.println(aluno02);
+	}
+
+}
