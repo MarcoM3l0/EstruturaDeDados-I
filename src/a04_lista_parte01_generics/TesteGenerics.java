a8f031e1c5d85b4dfe497e1b70649277d032d691
Marco Melo <josemarcomelonascimento@gmail.com>
2024-07-15 19:27:39 -0300

    feat: implementação métodos parametrizados

---
+++ src/a04_lista_parte01_generics/TesteGenerics.java
@@ -0,0 +1,54 @@
+package a04_lista_parte01_generics;
+
+public class TesteGenerics {
+
+	public static void main(String[] args) {
+		
+		Lista<Aluno> lista = new Lista<Aluno>();
+		
+		System.out.println("------- Teste Generalização -------");
+		System.out.println();
+		
+		lista.adicionar(new Aluno("Marco01", "Marco01@email.com"));
+		lista.adicionar(new Aluno("Marco02", "Marco02@email.com"));
+		lista.adicionar(new Aluno("Marco03", "Marco03@email.com"));
+//		lista.adicionar(new Professor("Marco03", "Marco03@email.com"));
+		lista.adicionar(new Aluno("Marco04", "Marco04@email.com"));
+		
+		System.out.println(lista);
+		System.out.println();
+		System.out.println("-----------------------------------");
+		
+		System.out.println(((Aluno) lista.pega(1)).nome.equals("Marco02"));
+//		System.out.println(((Professor) lista.pega(2)).nome.equals("Marco03"));
+		System.out.println();
+		System.out.println("-----------------------------------");
+		
+		Object elemento = lista.pega(2);
+		
+		if(elemento instanceof Aluno) {
+			System.out.println(((Aluno) elemento).nome.equals("Marco03"));
+		} else if(elemento instanceof Professor) {
+			System.out.println(((Professor) elemento).nome.equals("Marco03"));
+		}
+		
+		System.out.println();
+		System.out.println("-----------------------------------");
+		
+		System.out.println(lista);
+		lista.remover(1);
+		
+		System.out.println(lista);
+		lista.remover(2);
+		
+		System.out.println(lista);
+		lista.remover(2);
+		
+		System.out.println();
+		System.out.println("-----------------------------------");
+		
+		
+
+	}
+
+}
